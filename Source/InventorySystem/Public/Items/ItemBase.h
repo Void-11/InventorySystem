d03c1d6478e9a2b0c0a866dad07fe5eba93308d1
Void-11 <vivaanmishra777@gmail.com>
2024-03-16 23:58:12 +0530

    Added ItemBase Class

---
+++ Source/InventorySystem/Public/Items/ItemBase.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/ItemData.h"
+#include "ItemBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORYSYSTEM_API UItemBase : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	//Properties And Variables
+	
+	UPROPERTY(VisibleAnywhere, Category = "Item Data", meta = (UIMin=1, UIMax=100))
+	int32 Quantity;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FName ID;
+	
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemTextData TextData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemNumericData NumericData;
+
+	UPROPERTY(EditAnywhere, Category = "Item Data")
+	FItemAssetData AssetData;
+
+	//Functions
+
+	UItemBase();
+
+	UFUNCTION(Category = "Item")
+	UItemBase* CreateItemCopy();
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemStackWeight() const {	return Quantity * NumericData.Weight;	};
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float GetItemSingleWeight() const {	return Quantity * NumericData.Weight;	};
+
+	UFUNCTION(Category = "Item")
+	FORCEINLINE float IsFullItemStack() const {	return Quantity * NumericData.Weight;	};
+
+	UFUNCTION(Category = "Item")
+	void SetQuantity(const int32 NewQuantity);
+
+	UFUNCTION(Category = "Item")
+	virtual void Use(AInventorySystemCharacter* Character);
+};
