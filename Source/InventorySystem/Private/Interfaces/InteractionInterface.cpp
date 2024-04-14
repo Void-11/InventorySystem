c4ff737df7367a8103e23af3780b68f94deb9351
Void-11 <vivaanmishra777@gmail.com>
2024-04-14 23:59:34 +0530

    Modified TakePickup

---
+++ Source/InventorySystem/Private/Interfaces/InteractionInterface.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/InteractionInterface.h"
+#include "InventorySystemCharacter.h"
+
+// Add default functionality here for any IInteractionInterface functions that are not pure virtual.
+void IInteractionInterface::InitiateFocus()
+{
+}
+
+void IInteractionInterface::TerminateFocus()
+{
+}
+
+void IInteractionInterface::InitiateInteract()
+{
+}
+
+void IInteractionInterface::TerminateInteract()
+{
+}
+
+void IInteractionInterface::Interact(AInventorySystemCharacter* PlayerCharacter)
+{
+}
