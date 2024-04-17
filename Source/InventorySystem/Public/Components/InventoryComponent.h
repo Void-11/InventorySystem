6401690662e1a8a166dbad20c37f4004301a9881
Void-11 <vivaanmishra777@gmail.com>
2024-04-17 23:59:43 +0530

    Update InventoryComponent.h

--- Source/InventorySystem/Public/Components/InventoryComponent.h
+++ Source/InventorySystem/Public/Components/InventoryComponent.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Items/ItemBase.h"
 #include "InventoryComponent.generated.h"
 
 DECLARE_MULTICAST_DELEGATE(FOnInventoryUpdated);
