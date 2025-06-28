150a22715040aafe5ef32a22a07b02742a2b1a8c
Void-11 <vivaanmishra777@gmail.com>
2025-06-29 00:01:20 +0530

    Player Controller

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/IS_PlayerController.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/IS_PlayerController.h"
+
+#include "Inventory.h"
+
+void AIS_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogInventory, Log, TEXT("BeginPlay for PlayerController"))
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/IS_PlayerController.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "IS_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AIS_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+};
